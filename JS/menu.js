de9433c616fd139d002f51aacfa2c7f917f81a31
Olusola-Charles <64751824+Olusola-Charles@users.noreply.github.com>
2020-08-24 13:55:54 +0100

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('#menu-toggle');
+const navbarLeft = document.querySelector('#navbar-left');
+
+
+menuToggle.addEventListener('click', () => {
+    navbarLeft.classList.toggle('active');
+});
